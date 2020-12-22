e005d1e8519c27571e6675a3eb80a7a6d2253ca1
elizabeth <elizabethpt2020@ec.ajce.in>
2020-12-22 12:51:57 +0530

    neww

---
+++ javascriptprograms/functionalprograms/mapfilter.js
@@ -0,0 +1,14 @@
+var arr=[1,2,33,34,35]
+
+//Map()
+//filter()
+
+//su
+
+//var squ=arr.map(num=>num**2)
+//var even=arr.filter(num=>num%2==0)
+
+//reduce
+
+var maxs=arr.reduce((no1,no2)=>no1>no2?no1:no2)
+console.log(maxs)
